a1528e4a2d0a17eaaf7701ec804a7fcd4d588401
Abdul Rahman Daudzai <wadaudzai@gmail.com>
2023-02-28 22:30:52 -0500

    Make some changes in routes file to only create routes for two actions.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
+  resources :users, only: [:index, :show]
   resources :users do
-    resources :posts
+    resources :posts, only: [:index, :show]
   end
 end
