348598576b95d4a62c13c847b8be8d1dcd7be27a
Abdul Rahman Daudzai <wadaudzai@gmail.com>
2023-03-04 15:47:10 -0500

    Add the html files to create like and comment on the posts.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts, only: [:index, :show]
   end
+  resources :posts, only: [:new, :create]
+  resources :comments, only: [:new, :create]
+  resources :likes, only: [:new, :create]
 end
