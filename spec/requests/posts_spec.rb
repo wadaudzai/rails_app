8a6c68985c4ccdd8c03f326d0eac539b3e6199c5
Abdul Rahman Daudzai <wadaudzai@gmail.com>
2023-03-03 23:56:32 -0500

    Fix linter errors.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   before(:each) do
     @user = User.create(name: 'Mateo Lane', photo: 'https://unsplash.com/photos/mateopic',
                         bio: 'Comedian, Italian, stylish, and part time youtuber', posts_counter: 5)
@@ -8,35 +8,35 @@ RSpec.describe "Posts", type: :request do
                         likes_counter: 1)
   end
 
-  describe "request list of all posts" do
+  describe 'request list of all posts' do
     before(:each) { get user_posts_path(@user) }
 
-    it "Gives the correct response status" do
+    it 'Gives the correct response status' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "Renders the correct template" do
+    it 'Renders the correct template' do
       expect(response).to render_template('index')
     end
 
-    it "Checks for full list of posts info into the body" do
-      expect(response.body).to include("<h1>Here is a list of posts for a given user</h1>")
+    it 'Checks for full list of posts info into the body' do
+      expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
     end
   end
 
-  describe "Request only one post for a given user and post id" do
+  describe 'Request only one post for a given user and post id' do
     before(:each) { get user_post_path(@user, @post) }
 
-    it "Gives the correct response status" do
+    it 'Gives the correct response status' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "Renders the correct template succesfully" do
+    it 'Renders the correct template succesfully' do
       expect(response).to render_template('show')
     end
 
-    it "Checks for single post info into the body" do
-      expect(response.body).to include("<h1>Here is a list of posts for a given user</h1>")
+    it 'Checks for single post info into the body' do
+      expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
     end
   end
 end
